4e61d02592810574b623bae46f1425824248a3ab
DEKEL USACH <48386383@ort>
2025-03-27 08:31:35 -0300

    Finished project with extras

--- javascript.js
+++ javascript.js
@@ -7,21 +7,8 @@ const btnBorrar = document.querySelector("#btn-borrar")
 btnAdd.addEventListener("click", (event) => {
     if (document.querySelector(".form-control").value != "") {
         let tiempoActual = Date.now()
-        let tiempoActualHecho = new Date(tiempoActual)
-        let dia = tiempoActualHecho.getDate();
-        let mes = tiempoActualHecho.getMonth() + 1;
-        let año = tiempoActualHecho.getFullYear();
-        let hora = tiempoActualHecho.getHours();
-        let minutos = tiempoActualHecho.getMinutes();
-        let segundos = tiempoActualHecho.getSeconds();
-        let fechaYHora = 0;
-        if (segundos < 10) {
-            if (minutos < 10)
-                fechaYHora = `${dia}/${mes}/${año} ${hora}:${minutos}:0${segundos}`;
-        }
-        else {
-            fechaYHora = `${dia}/${mes}/${año} ${hora}:${minutos}:${segundos}`;
-        }
+        
+        let fechaYHora=  SacarFechaActual(tiempoActual)
 
 
 
@@ -34,7 +21,7 @@ btnAdd.addEventListener("click", (event) => {
             fecha: fechaYHora,
             status: 0,
             fechaTotal: tiempoActual,
-            finished: 0
+            finished: null
         })
         i++;
 
@@ -44,30 +31,28 @@ btnAdd.addEventListener("click", (event) => {
 
         // Tachado
         const checkboxs = document.querySelectorAll(".form-check-input")
-        console.log(checkboxs)
 
         checkboxs.forEach((element) => {
 
             element.addEventListener("change", () => {
                 let titulos = document.querySelectorAll(".tarea-titulo")
 
-                console.log(element.style.textDecoration)
 
                 if (element.checked) {
                     titulos[element.id].style.textDecoration = "line-through"
                     let tiempo = Date.now()
+                   
                     arrayTareas[element.id].finished = tiempo
                     arrayTareas[element.id].status = 1
-                    console.log(arrayTareas[element.id].finished)
 
                 }
                 else {
                     titulos[element.id].style.textDecoration = "none"
-
                     arrayTareas[element.id].status = 0
                 }
+               
             })
-
+            
         })
     }
     else {
@@ -85,16 +70,19 @@ btnAdd.addEventListener("click", (event) => {
 function MostrarTareas() {
     let contenido = document.querySelector(".tab-content");
     contenido.innerHTML = "";
-
+    
     arrayTareas.forEach(element => {
         if (element.status === 0) {
             contenido.innerHTML += `
             <div class="tab-pane fade show active">
                 <ul class="list-group mb-0">
+            
                     <li class="list-group-item d-flex align-items-center border-0 mb-2 rounded" style="background-color: #f4f6f7;">
+
                         <input class="form-check-input" type="checkbox" id="${element.id}" />
                         <p class="tarea-titulo" style="text-decoration: none;"> ${element.nombre}</p>
                         <div class="fecha"> ${element.fecha}</div>
+
                     </li>
                 </ul>
             </div>`;
@@ -107,6 +95,7 @@ function MostrarTareas() {
                         <input class="form-check-input" type="checkbox" id="${element.id}" checked/>
                         <p class="tarea-titulo" style="text-decoration: line-through;"> ${element.nombre}</p>
                         <div class="fecha"> ${element.fecha}</div>
+
                     </li>
                 </ul>
             </div>`;
@@ -143,8 +132,33 @@ btnBorrar.addEventListener("click", () => {
     let contenido = document.querySelector(".tab-content");
     arrayTareas = []
     contenido.innerHTML = ""
-
+    i = 0
     let tareaRapida = document.querySelector(".tareaRapida")
 
     tareaRapida.innerHTML = "¡Se ha borrado todo correctamente!"
+    setTimeout(() => {
+    tareaRapida.innerHTML = ""
+    }, 4000);
 })
+
+
+//Fecha actual
+function SacarFechaActual(tiempo){
+    let tiempoParseado = new Date(tiempo)
+    
+    let dia = tiempoParseado.getDate();
+    let mes = tiempoParseado.getMonth() + 1;
+    let año = tiempoParseado.getFullYear();
+    let hora = tiempoParseado.getHours();
+    let minutos = tiempoParseado.getMinutes();
+    let segundos = tiempoParseado.getSeconds();
+    let fechaYHora = 0;
+    if (segundos < 10) {
+        if (minutos < 10)
+            fechaYHora = `${dia}/${mes}/${año} ${hora}:${minutos}:0${segundos}`;
+    }
+    else {
+        fechaYHora = `${dia}/${mes}/${año} ${hora}:${minutos}:${segundos}`;
+    }
+    return fechaYHora
+}
